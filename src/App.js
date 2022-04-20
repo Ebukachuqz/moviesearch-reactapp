c58efd54574b1c927adc1ec9aa2cf8c1d4a5fb31
EbukaChuqz <ebukachuqz@gmail.com>
2022-04-20 15:58:36 +0100

    First Commit after building React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import MovieCard from './MovieCard'
 
-function App() {
+const omdbapi = 'http://www.omdbapi.com/?i=tt3896198&apikey=36ac6ee8'
+
+
+const App = () => {
+  const [search, setSearch] = useState('')
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    searchMovies('batman')
+  }, [])
+
+  const searchMovies = async (title) => {
+    const req = await axios.get(`${omdbapi}&s=${title}`)
+    setMovies(req.data.Search)
+    setSearch('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <h1>Movie Search</h1>
+        <div className="search">
+          <input
+            type="search"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          <i class="bi bi-search"
+          onClick={()=> searchMovies(search)}
+          ></i>
+        </div>
+      </div>
+
+      <div className="container">
+        {movies?.length > 0 ? (
+          movies.map((movie) => {
+          return <MovieCard movie={movie} key={movie.imdbID} />;
+          })
+        ) : (
+        <h1>No Movies</h1>
+        )}
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const MovieCard = ({movie}) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A" ? movie.Poster :
+            `https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101027/112815900-no-image-available-icon-flat-vector.jpg`
+          }
+          alt={movie.Title}
+        />
+      </div>
+
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard
